f96a9f5730b39ae80560c471f829018c02982098
Adam Nigon <Adam.j.nigon@gmail.com>
2015-02-13 15:58:03 -0700

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Nigon's on 2/13/15.
+//  Copyright (c) 2015 Adam Nigon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
